17ed5db52685c487e7342fd24d75cc086d016578
FMuniz312 <49133803+FMuniz312@users.noreply.github.com>
2021-01-24 19:06:34 -0300

    Proto 2
    Changed the throw mechanic to be "pull and release". I also changed the size of the wave.

--- Assets/Scripts/Systems/SYSTEMSDEBUG.cs
+++ Assets/Scripts/Systems/SYSTEMSDEBUG.cs
@@ -7,38 +7,42 @@ public class SYSTEMSDEBUG : MonoBehaviour
     [SerializeField] bool forceWind;
     [SerializeField] float windForce;
     [SerializeField] float windAngle;
-    [SerializeField] float forceAngle45Right;
-    [SerializeField] float forceAngle45Left;
-    [SerializeField] float forceAngle90;
-  
-       Vector2 windDirection;
+    [SerializeField] float maxPullDistance;
+    [SerializeField]float maxPullForce;
+    float forcemultiplier;
+
+    Vector2 windDirection;
     [SerializeField] GameObject tester;
     Vector3 originalPosition;
     ThrowObjectSystem throwObjectSystem;
 
+    [SerializeField] LayerMask throwableMask;
+    bool startPulling;
+     Vector3 mouseOnWorldPosition;
+    const float RAYCAST_RADIUS = 3;
+    Vector3 launchDirection;
+
     /* INSTRUCTIONS
-     * Press E to throw the ball 45° to the right of the boat
-     * Press Q to throw the ball 45° to the left of the boat
-     * Press W to throw the ball 90° straight up
-     * 
-     * Press SPACE to reset the ball position
-     * 
-     * bool forceWind is to set the wind effect on or off
-     * float windForce is the force that the wind apply on the ball's rigidbody
-     * float windAngle is the angle that the wind is pushing towards
-     * float forceAngle45Right, forceAngle45Left and forceAngle90 are the force amounts for each angle (it's a bit different from each other because 
-     *                                                                                                  of the wind force)
-     * GameObject tester is the ball itself
-     * 
-     * Feel free to tweek any options either from this debug script or from the Ball's rigidbody component so we can find
-     * the sweet spot
-     * 
-     * in case you want to change the angle to which the ball is thrown with E, Q or W. Then go to the Update function inside this script
-     * there's a function called ThrowObject() being used 3 times, the second parameter of this function is the angle. 
-     * However, be aware that unity counts the angle units counter-clockwise, angle 0° being straight right.
-     * 
-     * I added a helper logic as well, so we can use -45° instead of 135° (left 45°), which you can see it being used 
-     * in the Start() function of this script
+      TURN ON GIZMOS
+
+    Left mouse button start the pulling process, once you click it, hold it and move it around. 
+    A ray will show you the direction of the trajectory that the ball will follow once you release the
+    mouse button. The green circle following the mouse will show you how close you'll need to be to actually "pull the power" to charge
+     
+    Space resets the ball position
+   
+    
+
+    float maxPullDistance ---> max distance in world units
+    float maxPullForce ---> max possible force applied to the ball's rigidbody
+    float forcemultiplier ---> multiplier of the force applied to the ball's rigidbody so it can go from 0% to 100% of the maxForce
+    LayerMask throwableMask ---> LayerMask to focus the raycast
+    bool startPulling ---> boolean to start the calculation process
+    Vector3 mouseOnWorldPosition ---> holds the mouse position inside the world
+    const float RAYCAST_RADIUS = 3 ---> raycast radius. Also being show in the gizmos
+    Vector3 launchDirection ---> direction of the launch
+
+
    */
     private void Start()
     {
@@ -66,26 +70,9 @@ public class SYSTEMSDEBUG : MonoBehaviour
         {
             tester.GetComponent<Rigidbody2D>().AddForce(windDirection.normalized * windForce, ForceMode2D.Force);
         }
-        if (Input.GetKeyDown(KeyCode.E))
-        {
-            if (tester.GetComponent<Rigidbody2D>().constraints != RigidbodyConstraints2D.None) tester.GetComponent<Rigidbody2D>().constraints = RigidbodyConstraints2D.None;
-            throwObjectSystem.ThrowObject(tester, 45, forceAngle45Right);
-            forceWind = true;
-        }
-        if (Input.GetKeyDown(KeyCode.W))
-        {
-            if (tester.GetComponent<Rigidbody2D>().constraints != RigidbodyConstraints2D.None) tester.GetComponent<Rigidbody2D>().constraints = RigidbodyConstraints2D.None;
-            throwObjectSystem.ThrowObject(tester, 90, forceAngle90);
-            forceWind = true;
-        }
-        else if (Input.GetKeyDown(KeyCode.Q))
-        {
-            if (tester.GetComponent<Rigidbody2D>().constraints != RigidbodyConstraints2D.None) tester.GetComponent<Rigidbody2D>().constraints = RigidbodyConstraints2D.None;
-            throwObjectSystem.ThrowObject(tester, -45, forceAngle45Left);
-            forceWind = true;
-        }
 
-        else if (Input.GetKeyDown(KeyCode.Space))
+
+        if (Input.GetKeyDown(KeyCode.Space))
         {
             tester.transform.position = originalPosition;
             tester.GetComponent<Rigidbody2D>().constraints = RigidbodyConstraints2D.FreezePositionX;
@@ -93,6 +80,51 @@ public class SYSTEMSDEBUG : MonoBehaviour
 
         }
 
+        mouseOnWorldPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);//This won't be here when we build the actual system
+        //but,for debug/gizmos reasons, we are getting the information directly in the update
+
+        if (Input.GetMouseButtonDown(0))
+        {
+             RaycastHit2D hitInfo = Physics2D.CircleCast(mouseOnWorldPosition, RAYCAST_RADIUS, Vector2.zero,0, throwableMask);
+            if (hitInfo)
+            {
+                startPulling = true;
+
+            }
+        }
+        if (startPulling)
+        {
+           
+
+            //this part should not be in here, because we don't need to calculate all of this before we get in the next if statement
+            //However, for debug/gizmos reasons, we are doing the calculation here
+            float distance = Vector2.Distance(mouseOnWorldPosition, tester.transform.position);
+
+            if (distance > maxPullDistance) forcemultiplier = 1;
+            else
+            {
+                forcemultiplier = distance / maxPullDistance;
+            }
+
+            launchDirection = (tester.transform.position - mouseOnWorldPosition).normalized;
+            Debug.DrawRay(tester.transform.position, launchDirection * forcemultiplier* maxPullForce);
+            if (Input.GetMouseButtonUp(0))
+            {
+                startPulling = false;
+               
+                if (tester.GetComponent<Rigidbody2D>().constraints != RigidbodyConstraints2D.None) tester.GetComponent<Rigidbody2D>().constraints = RigidbodyConstraints2D.None;
+                throwObjectSystem.ThrowObject(tester, launchDirection, forcemultiplier* maxPullForce);
+                forceWind = true;
+            }
+
+        }
+       
+
     }
 
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawWireSphere(mouseOnWorldPosition, RAYCAST_RADIUS);
+        
+    }
 }
