82646d40776b39904f0c4c263d997fb5388d3e6a
Tadeas <tstudiogames2@gmail.com>
2021-01-24 12:22:32 +0100

    Boat and wave scripts

---
+++ Assets/Scripts/Behaviors/Boat.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Boat : MonoBehaviour
+{
+    [Header("General")]
+    public float speed = 5;
+    [Header("Physics")]
+    public Transform centerOfMass;
+    //private stuff
+    private Vector3 startCamPos;
+    // Start is called before the first frame update
+    void Start()
+    {
+        GetComponent<Rigidbody2D>().centerOfMass = centerOfMass.localPosition;
+        startCamPos = Camera.main.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    public void Boost(float boostValue, float boostTime)
+    {
+        StartCoroutine(BoostCoruntine(boostValue, boostTime));
+    }
+
+    public IEnumerator BoostCoruntine(float boostValue, float boostTime)
+    {
+        speed += boostValue;
+        Camera.main.transform.position = startCamPos + new Vector3(-1f, 0, 0);
+        GetComponent<Rigidbody2D>().centerOfMass = centerOfMass.localPosition - new Vector3(0.5f, 0, 0);
+        yield return new WaitForSeconds(boostTime);
+        speed -= boostValue;
+        GetComponent<Rigidbody2D>().centerOfMass = centerOfMass.localPosition;
+        Camera.main.transform.position = startCamPos;
+    }
+}
---
+++ Assets/Scripts/Behaviors/Wave.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Wave : MonoBehaviour
+{
+    private Boat boat;
+    public bool boatRidingNow;
+    public float timeOnTheWaveToFall = 1;
+    private float timeOnTheWave;
+    // Start is called before the first frame update
+    void Start()
+    {
+        boat = GameObject.FindGameObjectWithTag("Boat").GetComponent<Boat>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Time.deltaTime * boat.speed * -1, 0, 0);
+
+        if (boatRidingNow)
+        {
+            if(timeOnTheWave > timeOnTheWaveToFall)
+            {
+                boat.GetComponent<Rigidbody2D>().constraints = RigidbodyConstraints2D.None;
+                timeOnTheWave = 0;
+                boat.speed = 0;
+            } else
+            {
+                timeOnTheWave += Time.deltaTime;
+            }
+        } else
+        {
+            timeOnTheWave = 0;
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.tag == "Boat")
+        {
+            boatRidingNow = true;
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.tag == "Boat")
+        {
+            boatRidingNow = false;
+        }
+    }
+}
