fe7d59dd8d9b0a254e7c9c86644b924475aab81b
Esben <esbennj@hotmail.com>
2021-01-29 23:59:40 +0100

    game is good

---
+++ HowToBoat/BlackthornprodGameJam2021_Data/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
@@ -0,0 +1,92 @@
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+// Audiokinetic Wwise generated include file. Do not edit.
+//
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+
+#ifndef __WWISE_IDS_H__
+#define __WWISE_IDS_H__
+
+#include <AK/SoundEngine/Common/AkTypes.h>
+
+namespace AK
+{
+    namespace EVENTS
+    {
+        static const AkUniqueID MUSIC_GAME = 1072299391U;
+        static const AkUniqueID MUSIC_LOSE = 3736367630U;
+        static const AkUniqueID MUSIC_WIN = 2682680661U;
+        static const AkUniqueID NPC_COLLBIRD = 3321089774U;
+        static const AkUniqueID NPC_COLLFLOOR = 495534887U;
+        static const AkUniqueID NPC_COLLSHARK = 2474133806U;
+        static const AkUniqueID NPC_COLLWATER = 139855160U;
+        static const AkUniqueID NPC_COLLWATERMULTIPLENPCS = 3101144128U;
+        static const AkUniqueID NPC_DROPPEDBYBIRD = 2016968455U;
+        static const AkUniqueID NPC_FLY_BOY = 1811951503U;
+        static const AkUniqueID NPC_FLY_GIRL = 1765057129U;
+        static const AkUniqueID NPC_FLY_GRANDPA = 314326094U;
+        static const AkUniqueID NPC_FLY_MAN = 1037768413U;
+        static const AkUniqueID NPC_FLY_WOMAN = 3522606991U;
+        static const AkUniqueID NPC_IMPACT_CHILD = 1661441752U;
+        static const AkUniqueID NPC_IMPACT_GRANDPA = 1900293553U;
+        static const AkUniqueID NPC_IMPACT_MAN = 1533334522U;
+        static const AkUniqueID NPC_IMPACT_WOMAN = 651276332U;
+        static const AkUniqueID NPC_PICKEDUP = 2050233766U;
+        static const AkUniqueID NPC_PREPAREDFORTHROW = 4146794365U;
+        static const AkUniqueID NPC_THROWN = 2756003563U;
+        static const AkUniqueID PLAYER_FOOTSTEPS = 1730208058U;
+        static const AkUniqueID START_AMBIENCE = 3626999974U;
+        static const AkUniqueID START_MUSIC = 540993415U;
+        static const AkUniqueID WAVE_CLEARED = 1973806823U;
+        static const AkUniqueID WAVE_ENTER = 264499973U;
+        static const AkUniqueID WAVE_LOOP_BIG = 141150188U;
+        static const AkUniqueID WAVE_LOOP_MEDIUM = 429804171U;
+        static const AkUniqueID WAVE_LOOP_SMALL = 2751120299U;
+        static const AkUniqueID WAVE_LOOP_STOP = 2133808190U;
+        static const AkUniqueID WAVE_LOST = 2761884803U;
+    } // namespace EVENTS
+
+    namespace STATES
+    {
+        namespace GAMESTATE
+        {
+            static const AkUniqueID GROUP = 4091656514U;
+
+            namespace STATE
+            {
+                static const AkUniqueID GAME = 702482391U;
+                static const AkUniqueID LOSE = 221232726U;
+                static const AkUniqueID NONE = 748895195U;
+                static const AkUniqueID WIN = 979765101U;
+            } // namespace STATE
+        } // namespace GAMESTATE
+
+    } // namespace STATES
+
+    namespace GAME_PARAMETERS
+    {
+        static const AkUniqueID BOATSPEED = 659411096U;
+        static const AkUniqueID DISTANCETOWAVE = 295969768U;
+        static const AkUniqueID THROWPOWER = 2111892502U;
+    } // namespace GAME_PARAMETERS
+
+    namespace BANKS
+    {
+        static const AkUniqueID INIT = 1355168291U;
+        static const AkUniqueID SOUNDBANK = 1661994096U;
+    } // namespace BANKS
+
+    namespace BUSSES
+    {
+        static const AkUniqueID MASTER_AUDIO_BUS = 3803692087U;
+    } // namespace BUSSES
+
+    namespace AUDIO_DEVICES
+    {
+        static const AkUniqueID NO_OUTPUT = 2317455096U;
+        static const AkUniqueID SYSTEM = 3859886410U;
+    } // namespace AUDIO_DEVICES
+
+}// namespace AK
+
+#endif // __WWISE_IDS_H__
